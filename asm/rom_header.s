f126e1141c289d2fe5323837892f7f05fcdfa47f
sunlitspace542 <rabbidsluis123@gmail.com>
2023-11-05 19:39:21 -0500

    add savetype to rom header

--- asm/rom_header.s
+++ asm/rom_header.s
@@ -1,5 +1,5 @@
 /*
- * Super Mario 64 ROM header
+ * ROM header
  * Only the first 0x18 bytes matter to the console.
  */
  
@@ -27,18 +27,13 @@
 # .if USE_GC == 1
 .if  1
 /* Advanced homebrew ROM header bytes: https://n64brew.dev/wiki/ROM_Header#Advanced_Homebrew_ROM_Header */
-.word  0x82000000
+.word  0x82000000               /* Expected controllers */
 .else
-.word  0x00000000               /* Unknown */
+.word  0x00000000               /* Expected controllers */
 .endif
 .word  0x0000004E               /* Cartridge */
 
-.if 0
-# .if EEP4K == 1 && USE_GC == 0
-.ascii "SM"                     /* Cartridge ID */
-.else
 .ascii "ED"                     /* Cartridge ID */
-.endif
 
 /* Region */
 .if VERSION_EU == 1
@@ -49,10 +44,4 @@
     .ascii "E"                  /* NTSC-U (North America) */
 .endif
 
-/* Undefined versions 1 and 2 are NTSC-U (North America) and PAL (Europe) respectively */
-.if VERSION_SH == 1
-    .byte 0x03                  /* Version (Shindou) */
-.else
-    .byte 0x00                  /* Version */
-.endif
-
+    .byte 0x10                  /* Savetype - 0x10 = EEP4K */
